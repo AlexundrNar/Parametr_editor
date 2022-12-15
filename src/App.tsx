55f6910287896a762dd9f57e69d5dd2b68d38d2d
NarAlex <sashabik@mail.ru>
2022-12-15 17:54:08 +0600

    param editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+  params: Param[];
+  model: Model;
 }
 
-export default App;
+interface Param {
+  id: number;
+  name: string;
+  type?: "string" | "number" | "[]";
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  //colors: Color[];             - Здесь массив цветов никуда не ведет. Не уверен был, надо ли было мне доп. параметры делать? Оставил комментом
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+type State = {
+  model: Model
+};
+
+const params = [
+  {
+    id: 1,
+    name: "Назначение",
+  },
+  {
+    id: 2,
+    name: "Длина",
+  },
+];
+
+const model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное",
+    },
+    {
+      paramId: 2,
+      value: "макси",
+    },
+  ],
+};
+
+class ParamEditor extends React.Component<Props, State> {
+  params: Param[]
+  model: Model
+  valInput: React.RefObject<HTMLInputElement>;
+  
+  constructor(props: Props) {
+    super(props)
+    this.params = params
+    this.model = model
+    this.state = {model: model}
+    this.valInput = React.createRef()
+  }
+    
+
+  public getModel(): Model {
+    return this.state.model;
+  }
+
+  render() {
+    return (
+      <div className="main">
+          <div className="list">
+            {this.params.map(param => <div key={param.id}>
+              <span>{param.name}</span>
+            </div>)}
+          </div>
+
+          <div>
+            {this.model.paramValues.map(model => <div key={model.paramId}>
+              <input defaultValue={model.value} ref={this.valInput} onChange={this.getModel} />
+            </div>)}
+          </div>
+      </div>
+    );
+  }
+}
+
+export default ParamEditor;
